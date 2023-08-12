2dda0f539f4689e07f1800b0e5fb06ba7a51caac
appuser <appuser@journey2-86bff4fbf7-g2r44>
2023-08-12 08:10:05 +0000

    starter project

---
+++ client/src/Components/EmployeeForm/EmployeeForm.jsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+
+const EmployeeForm = ({ onSave, disabled, employee, onCancel }) => {
+  const [name, setName] = useState(employee?.name ?? "");
+  const [level, setLevel] = useState(employee?.level ?? "");
+  const [position, setPosition] = useState(employee?.position ?? "");
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    if (employee) {
+      return onSave({
+        ...employee,
+        name,
+        level,
+        position,
+      });
+    }
+
+    return onSave({
+      name,
+      level,
+      position,
+    });
+  };
+
+  return (
+    <form className="EmployeeForm" onSubmit={onSubmit}>
+      <div className="control">
+        <label htmlFor="name">Name:</label>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          name="name"
+          id="name"
+        />
+      </div>
+
+      <div className="control">
+        <label htmlFor="level">Level:</label>
+        <input
+          value={level}
+          onChange={(e) => setLevel(e.target.value)}
+          name="level"
+          id="level"
+        />
+      </div>
+
+      <div className="control">
+        <label htmlFor="position">Position:</label>
+        <input
+          value={position}
+          onChange={(e) => setPosition(e.target.value)}
+          name="position"
+          id="position"
+        />
+      </div>
+
+      <div className="buttons">
+        <button type="submit" disabled={disabled}>
+          {employee ? "Update Employee" : "Create Employee"}
+        </button>
+
+        <button type="button" onClick={onCancel}>
+          Cancel
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default EmployeeForm;
